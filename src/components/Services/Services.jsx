806f14aea6034dc4f3173f2651b7b9626620a453
Muhammad Talha shamim <ts083020@gmail.com>
2023-01-12 07:14:54 +0500

    1/12/2023

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,10 +1,11 @@
 
 import React from 'react'
 import './Services.css'
-import HeartEmoji from "../../img/heartemoji.png"
-import Glasses from "../../img/glasses.png"
-import Humble from "../../img/humble.png"
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
 import Card from '../Card/Card';
+import Resume from '../../img/resume.pdf';
 const Services = () => {
   return (
     <div className='Services'>
@@ -15,18 +16,33 @@ const Services = () => {
                 <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 <br />
                 Itaque sunt voluptate quisquam totam officia saepe?</span>
-                <button className="button s-button">Download CV </button>
+                <a href={Resume} ><button className="button s-button">Download CV </button></a>
+                
                 <div className="blur s-blur" style={{Background:"#ABF1FF94"}}></div>
             </div>
       {/* right silde  */}
-      <div className="card">
-               <div>
+      <div className="cards">
+               <div style={{left:'40rem'}}>
                 <Card 
                  emoji={HeartEmoji}
                  heading={'Design'}
                  detail={'Figma,sketch,photshop,Adob,Adob xd'}
                 />
                 </div> 
+                <div style={{top:'12rem', left:'17rem'}}>
+                <Card 
+                 emoji={Glasses}
+                 heading={'Developer'}
+                 detail={'Html,Css,Javascript,React js'}
+                />
+                </div>
+                <div style={{top:'20rem', left:'35rem'}}>
+                <Card 
+                 emoji={Humble}
+                 heading={'UI/UX'}
+                 detail={'Html,Css,Javascript,React js'}
+                />
+                </div>
       </div>
     </div>
   )
