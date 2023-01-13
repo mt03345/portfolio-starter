dd6626a0eed1783249a9687dc13229187932a3ba
Muhammad Talha shamim <ts083020@gmail.com>
2023-01-13 07:00:07 +0500

    13 jan fri

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Portfolio.css'
+import {Swiper,SwiperSlide} from 'swiper/react'
+const Portfolio = () => {
+  return (
+    <div className="Portfolio">
+        heading
+    </div>
+  )
+}
+
+export default Portfolio
